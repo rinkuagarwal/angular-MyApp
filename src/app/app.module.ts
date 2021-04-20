1910c58159e729f5dd94075d1ceb32f4f62089fa
unknown <ibmjavanh16@iiht.tech>
2021-04-20 11:13:29 +0530

    navigation added and pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,25 @@ import{ FormsModule }from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserformComponent } from './userform/userform.component';
+import { SearchComponent } from './search/search.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: '', component: UserformComponent }, //default, Home page
+  { path: 'search', component: SearchComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserformComponent
+    UserformComponent,
+    SearchComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule, FormsModule,HttpClientModule,
   ],
   providers: [],
